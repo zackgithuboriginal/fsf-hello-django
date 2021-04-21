ea47e4cc70fe5573abe765ba2cda5fab58b4e970
zackgithuboriginal <z.gibbons@hotmail.com>
2021-04-21 14:18:34 +0000

    *

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -30,11 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-if development:
-    ALLOWED_HOSTS = ['localhost']
-else:
-    ALLOWED_HOSTS = ['zackg-django-todo-app.herokuapp.com/']
-
+ALLOWED_HOSTS = ['zackg-django-todo-app.herokuapp.com', 'localhost']
 
 # Application definition
 
